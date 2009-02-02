178fc037a637b1fa3c6ff823d87bfe88c6153c38
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-02-02 21:01:00 +0000

    - tidy up the tests

--- t/01_queue.rb
+++ t/01_queue.rb
@@ -2,6 +2,19 @@ require 'test/unit'
 require 'priority_queue'
 
 class Test_PQueue < Test::Unit::TestCase
+    attr_accessor :tq
+
+    def setup
+        @tq = Queue::Priority.new()
+        @tq.push('hello', 1)
+        @tq.push('goodbye', 3)
+        @tq.push('lunchtime', 2)
+    end
+
+    def teardown
+        @tq = nil
+    end
+
     def test_init
         pq = Queue::Priority.new()
         assert(pq, 'create a Priority Queue')
@@ -20,41 +33,31 @@ class Test_PQueue < Test::Unit::TestCase
     end
 
     def test_sorted
-        pq = Queue::Priority.new()
-        pq.push('hello', 1)
-        assert_equal(false, pq.sorted?)
+        assert_equal(false, @tq.sorted?)
     end
 
     def test_priority_order
-        pq = Queue::Priority.new()
-        pq.push('hello', 1)
-        pq.push('goodbye', 3)
-        pq.push('lunchtime', 2)
-        assert_equal(false, pq.sorted?)
-        x = pq.pop()
+        assert_equal(false, @tq.sorted?)
+        x = @tq.pop()
         assert_equal('goodbye', x, 'p3 is the highest')
-        assert_equal(true, pq.sorted?, 'pop sorts the queue')
-        x = pq.pop()
+        assert_equal(true, @tq.sorted?, 'pop sorts the queue')
+        x = @tq.pop()
         assert_equal('lunchtime', x, 'p2 is second')
-        x = pq.pop()
+        x = @tq.pop()
         assert_equal('hello', x, 'p1 is last')
-        assert_equal(true, pq.sorted?, 'pop sorts the queue')
+        assert_equal(true, @tq.sorted?, 'pop sorts the queue')
     end
 
     def test_priority_reverse_order
-        pq = Queue::Priority.new()
-        pq.push('hello', 1)
-        pq.push('goodbye', 3)
-        pq.push('lunchtime', 2)
-        assert_equal(false, pq.sorted?)
-        x = pq.shift()
+        assert_equal(false, @tq.sorted?)
+        x = @tq.shift()
         assert_equal('hello', x, 'p1 is first')
-        assert_equal(true, pq.sorted?, 'shift sorts the queue')
-        x = pq.shift()
+        assert_equal(true, @tq.sorted?, 'shift sorts the queue')
+        x = @tq.shift()
         assert_equal('lunchtime', x, 'p2 is second')
-        x = pq.shift()
+        x = @tq.shift()
         assert_equal('goodbye', x, 'p3 is last')
-        assert_equal(true, pq.sorted?, 'shift sorts the queue')
+        assert_equal(true, @tq.sorted?, 'shift sorts the queue')
     end
 
     def test_array
